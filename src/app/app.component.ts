ac50ac3b15cf3b0130aa73b78618f6ddea97a9fe
Ajsal2718 <muhammedajsal532@gmail.com>
2023-10-21 11:58:08 -0800

    Component commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles: [`
+  h1{
+    text-align:center;
+    color:white;
+  }`]
+})
+export class AppComponent {
+  title = 'Components-Angular';
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector : 'app-home',
+  templateUrl : './home.component.html',
+  styles : [`
+  h2{
+    text-align:center;
+  }
+  .main-div{
+    text-align : center;  
+  }`]
+})
+
+
+export class HomeComponent {}
